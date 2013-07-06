c4b13b62824c1f00d807034ec4fb26eb1137ec6f
James Richardson <me+github@james.richardson.name>
2013-07-06 21:01:25 +0100

    added copyright notices

--- src/main/java/name/richardson/james/bukkit/jchat/jChat.java
+++ src/main/java/name/richardson/james/bukkit/jchat/jChat.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
- * Copyright (c) 2012 James Richardson.
- *
- * jChat.java is part of jChatPlugin.
- *
- * jChatPlugin is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * jChatPlugin is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * jChatPlugin. If not, see <http://www.gnu.org/licenses/>.
+ Copyright (c) 2013 James Richardson.
+
+ jChat.java is part of jChat.
+
+ jChat is free software: you can redistribute it and/or modify it
+ under the terms of the GNU General Public License as published by the Free
+ Software Foundation, either version 3 of the License, or (at your option) any
+ later version.
+
+ jChat is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with
+ jChat. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package name.richardson.james.bukkit.jchat;
 
